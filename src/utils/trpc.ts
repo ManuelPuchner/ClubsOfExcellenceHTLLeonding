54be2b21649f6c602136f69e26776dfad3116e4c
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-10-20 23:25:04 +0200

    fix

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,17 +1,21 @@
 // src/utils/trpc.ts
-import { httpBatchLink, loggerLink } from "@trpc/client";
+import { httpBatchLink, httpLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "../server/trpc/router";
 import superjson from "superjson";
 
 export const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${process.env.BASE_PATH}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}${process.env.BASE_PATH}`; // dev SSR should use localhost
+  let baseUrl = "";
+  if (typeof window !== "undefined") baseUrl = window.location.origin; // browser should use relative url
+  else if (process.env.VERCEL_URL) baseUrl = `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  else baseUrl = `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  return baseUrl;
 };
 
 export const trpc = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
+    const url = getBaseUrl() + process.env.NEXT_PUBLIC_BASE_PATH + "/api/trpc";
+    console.log("url", url);
     return {
       transformer: superjson,
       links: [
@@ -21,7 +25,7 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url,
         }),
       ],
     };
